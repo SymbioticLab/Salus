b24070ab2eaac566ee70c57a2b8a3a1b4f15ca84
Aetf <aetf@unlimitedcodeworks.xyz>
2017-06-28 00:27:38 -0400

    Use timeit for timing

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,24 +1,35 @@
 import tensorflow as tf
-from timeit import default_timer
+from timeit import default_timer, timeit
 
 config = tf.ConfigProto()
-config.graph_options.optimizer_options.opt_level = tf.OptimizerOptions.L0
+
+def build_graph(x):
+    tf.reset_default_graph()
+    m = tf.matmul(tf.random_normal((x,x)), tf.random_normal((x,x)))
+    return tf.reduce_sum(m)
+
+def time_on(dev, x):
+    print('Run on {}'.format(dev))
+    with tf.device('/device:' + dev + ':0'):
+        op = build_graph(x)
+        with tf.Session(config=config) as sess:
+            sess.run(op)  # warm up
+            st = default_timer()
+            rep = 10
+            for _ in range(rep):
+                sess.run(op)
+            dur = (default_timer() - st) / rep
+            print('Used time: {}s'.format(dur))
+
+def gpu(x):
+    return time_on('GPU', x)
 
 def cpu(x):
-    tf.reset_default_graph()
-    with tf.device('/device:CPU:0'):
-        with tf.Session(config=config) as sess:
-            st = default_timer()
-            tf.matmul(tf.random_normal((x,x)), tf.random_normal((x,x))).eval()
-            print(default_timer() - st)
+    return time_on('CPU', x)
 
 def rpc(x):
-    tf.reset_default_graph()
-    with tf.device('/device:RPC:0'):
-        with tf.Session(config=config) as sess:
-            st = default_timer()
-            tf.matmul(tf.random_normal((x,x)), tf.random_normal((x,x))).eval()
-            print(default_timer() - st)
+    return time_on('RPC', x)
 
+gpu(200)
 cpu(200)
 rpc(200)
