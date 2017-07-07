c32a857abb04abe0a23363a6358adf2eeda192ea
Peifeng Yu <peifeng@umich.edu>
2017-07-07 14:57:09 -0400

    Test add and multiply for more dtypes

--- tests/test_ops_tf.py
+++ tests/test_ops_tf.py
@@ -6,6 +6,7 @@ import numpy as np
 import numpy.testing as npt
 from ddt import ddt, data, unpack
 
+@ddt
 class TestBasicOps(unittest.TestCase):
 
     @classmethod
@@ -36,7 +37,7 @@ class TestBasicOps(unittest.TestCase):
             expected = tf.random_normal([20, 20], seed=seed)
 
         with tf.Session(config=self.config) as sess:
-            npt.assert_array_equal(sess.run(actual), sess.run(expected))
+            npt.assert_allclose(sess.run(actual), sess.run(expected), rtol=1e-6)
 
     def test_noop(self):
         with tf.device('/device:RPC:0'):
@@ -49,7 +50,7 @@ class TestBasicOps(unittest.TestCase):
     @unittest.skip("Segfault in GPU. See #12.")
     def test_multiply_int32(self):
         with tf.device('/device:RPC:0'):
-            a = tf.constant([3, 7], name='const_1')
+            a = tf.constant([3, 7], name='const_1', dtype=tf.int32)
             b = tf.constant([7, 3] , name='const_2')
             c = tf.constant(2, name='const_3')
             d = tf.multiply(a, b, name='mul_first')
@@ -65,8 +66,26 @@ class TestBasicOps(unittest.TestCase):
         with tf.Session(config=self.config) as sess:
             npt.assert_array_equal(sess.run(actual), sess.run(expected))
 
-    @data(tf.bool, tf.float32, tf.double, tf.int64, tf.uint8, tf.int8, tf.uint16, tf.int16,
-          tf.complex64, tf.complex128)
+    @data(tf.int8, tf.int16)
+    def test_multiply_shortint(self, dtype):
+        with tf.device('/device:RPC:0'):
+            a = tf.constant([3, 7], name='const_1', dtype=dtype)
+            b = tf.constant([7, 3] , name='const_2', dtype=dtype)
+            c = tf.constant(2, name='const_3', dtype=dtype)
+            d = tf.multiply(a, b, name='mul_first')
+            actual = tf.multiply(c, d, name='mul_second')
+
+        with tf.device('/device:CPU:0'):
+            a = tf.constant([3, 7], name='const_1', dtype=dtype)
+            b = tf.constant([7, 3] , name='const_2', dtype=dtype)
+            c = tf.constant(2, name='const_3', dtype=dtype)
+            d = tf.multiply(a, b, name='mul_first')
+            expected = tf.multiply(c, d, name='mul_second')
+
+        with tf.Session(config=self.config) as sess:
+            npt.assert_array_equal(sess.run(actual), sess.run(expected))
+
+    @data(tf.float32, tf.double, tf.int64, tf.complex64, tf.complex128)
     def test_multiply(self, dtype):
         with tf.device('/device:RPC:0'):
             a = tf.constant([3, 7], name='const_1', dtype=dtype)
@@ -85,8 +104,7 @@ class TestBasicOps(unittest.TestCase):
         with tf.Session(config=self.config) as sess:
             npt.assert_array_equal(sess.run(actual), sess.run(expected))
 
-    @data(tf.bool, tf.float32, tf.double, tf.int64, tf.uint8, tf.int8, tf.uint16, tf.int16,
-          tf.complex64, tf.complex128)
+    @data(tf.float32, tf.double, tf.int64, tf.complex64, tf.complex128)
     def test_add(self, dtype):
         with tf.device('/device:RPC:0'):
             a = tf.constant([3, 7], name='const_1', dtype=dtype)
@@ -104,6 +122,54 @@ class TestBasicOps(unittest.TestCase):
 
         with tf.Session(config=self.config) as sess:
             npt.assert_array_equal(sess.run(actual), sess.run(expected))
+
+    @unittest.skip("Segfault in GPU.")
+    @data(tf.int16, tf.int8)
+    def test_add_shortint(self, dtype):
+        with tf.device('/device:RPC:0'):
+            a = tf.constant([3, 7], name='const_1', dtype=dtype)
+            b = tf.constant([7, 3] , name='const_2', dtype=dtype)
+            c = tf.constant(2, name='const_3', dtype=dtype)
+            d = tf.add(a, b, name='add_first')
+            actual = tf.add(c, d, name='add_second')
+
+        with tf.device('/device:CPU:0'):
+            a = tf.constant([3, 7], name='const_1', dtype=dtype)
+            b = tf.constant([7, 3] , name='const_2', dtype=dtype)
+            c = tf.constant(2, name='const_3', dtype=dtype)
+            d = tf.add(a, b, name='mul_first')
+            expected = tf.add(c, d, name='add_second')
+
+        with tf.Session(config=self.config) as sess:
+            npt.assert_array_equal(sess.run(actual), sess.run(expected))
+
+    def test_matmul(self):
+        m1 = np.random.normal(size=(20, 50))
+        m2 = np.random.normal(size=(50, 60))
+        with tf.device('/device:RPC:0'):
+            actual = tf.matmul(m1, m2)
+        with tf.device('/device:CPU:0'):
+            expected = tf.matmul(m1, m2)
+
+        with tf.Session(config=self.config) as sess:
+            npt.assert_array_equal(sess.run(actual), sess.run(expected))
+
+    def test_conv2d(self):
+        with tf.device('/device:RPC:0'):
+            image = tf.constant([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], shape=(1, 4, 3, 1), dtype=tf.float32)
+            filter = tf.constant([1, 4, 7, 2, 5, 8, 3, 6, 9], shape=(3, 3, 1, 1), dtype=tf.float32)
+            actual = tf.nn.conv2d(image, filter, [1, 1, 1, 1], 'SAME')
+        with tf.device('/device:CPU:0'):
+            image = tf.constant([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], shape=(1, 4, 3, 1), dtype=tf.float32)
+            filter = tf.constant([1, 4, 7, 2, 5, 8, 3, 6, 9], shape=(3, 3, 1, 1), dtype=tf.float32)
+            expected = tf.nn.conv2d(image, filter, [1, 1, 1, 1], 'SAME')
+
+        with tf.Session(config=self.config) as sess:
+            npt.assert_array_equal(sess.run(actual), sess.run(expected))
+
+
+if __name__ == '__main__':
+    unittest.main()
 
     def test_matmul(self):
         m1 = np.random.normal(size=(20, 50))
