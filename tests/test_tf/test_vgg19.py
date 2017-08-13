644d7fee8c8ece18a73342412b63d60b74bbd771
Aetf <aetf@unlimitedcodeworks.xyz>
2017-08-12 20:19:48 -0400

    Fix queue runner

--- tests/test_tf/test_vgg19.py
+++ tests/test_tf/test_vgg19.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 
 import unittest
+from datetime import datetime
+from timeit import default_timer
 
 import tensorflow as tf
 
@@ -9,27 +11,44 @@ from . import networks, datasets
 
 
 def run_vgg19(sess, input_data):
-    images, labels, num_classes = input_data(batch_size=100, batch_num=None)
+    batch_size = 100
+    images, labels, num_classes = input_data(batch_size=batch_size, batch_num=100)
     train_mode = tf.placeholder(tf.bool)
 
     vgg = networks.Vgg19Trainable()
     vgg.build(images, train_mode)
 
     # print number of variables used: 143667240 variables, i.e. ideal size = 548MB
-    print(vgg.get_var_count())
+    print('Total number of variables: {}'.format(vgg.get_var_count()))
 
     cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=vgg.prob))
+        tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=vgg.prob))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
     correct_prediction = tf.equal(tf.argmax(vgg.prob, 1), tf.argmax(labels, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     sess.run(tf.global_variables_initializer())
+    coord = tf.train.Coordinator()
+    queue_threads = tf.train.start_queue_runners(sess, coord)
+    speeds = []
     for i in range(50):
+        start_time = default_timer()
         sess.run(train_step, feed_dict={train_mode: True})
+        duration = default_timer() - start_time
 
-    return sess.run(accuracy, feed_dict={train_mode: False})
+        examples_per_sec = batch_size / duration
+        sec_per_batch = float(duration)
+        speeds.append(examples_per_sec)
+        loss_value = sess.run(cross_entropy, feed_dict={train_mode: True})
+        fmt_str = '{}: step {}, loss = {:.2f} ({:.1f examples/sec; {:.3f} sec/batch'
+        print(fmt_str.format(datetime.now(), i, loss_value, examples_per_sec, sec_per_batch))
+
+    final_acc = sess.run(accuracy, feed_dict={train_mode: False})
+    coord.request_stop()
+    coord.join(queue_threads)
+
+    return final_acc
 
 
 class TestVGG19(unittest.TestCase):
