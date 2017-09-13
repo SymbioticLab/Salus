216edd7e2c29397bee18fe9f2bf0179d80c76730
Peifeng Yu <peifeng@umich.edu>
2017-09-13 16:02:45 +0000

    Collect.sh

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -3,10 +3,11 @@
 #-------------------------------------------------------
 # Settings
 #-------------------------------------------------------
-OUTPUT_DIR=$HOME/vgg16-rpc-gpu
-CASE='test_tf.test_vgg'
-RPC_ONLY_METHOD='TestVgg16.test_rpc_only'
-BOTH_METHOD='TestVgg16.test_fake_data'
+OUTPUT_DIR=$HOME/vgg-rpc-double
+PACKAGE='test_tf.test_vgg'
+CASE='TestVgg'
+RPC_ONLY_METHOD='test_rpc_only'
+BOTH_METHOD='test_fakedata'
 
 PYTHON=$HOME/.local/venvs/tfbuild/bin/python
 
@@ -20,7 +21,7 @@ EXEC_DIR=$DIR/..
 
 function run_client() {
     pushd $EXEC_DIR/tests > /dev/null
-    env TF_CPP_MIN_LOG_LEVEL=3 CUDA_VISIBLE_DEVICES=0,1 $PYTHON -m $CASE $1 | tee "$2"
+    env TF_CPP_MIN_LOG_LEVEL=3 CUDA_VISIBLE_DEVICES=0,1 $PYTHON -m $PACKAGE $1 | tee "$2"
     popd > /dev/null
 }
 
@@ -32,25 +33,29 @@ function profile_server() {
     env TF_CPP_MIN_LOG_LEVEL=4 CUDA_VISIBLE_DEVICES=2,3 nvprof --export-profile "$1" --events active_warps -- $EXEC_DIR/build/src/executor &
 }
 
-rm -rf "$OUTPUT_DIR" && mkdir -p "$OUTPUT_DIR"
+if [ -d "$OUTPUT_DIR" ]; then
+    echo "Output directory already exists: $OUTPUT_DIR"
+fi
+
+mkdir -p "$OUTPUT_DIR"
 cd $EXEC_DIR
 
 # Collect memory usage
 
 run_server "$OUTPUT_DIR/exec.output"
-run_client $RPC_ONLY_METHOD "$OUTPUT_DIR/mem-iter.log"
+run_client $CASE.$RPC_ONLY_METHOD "$OUTPUT_DIR/mem-iter.log"
 kill $(jobs -p)
 
 # Collect JCT
 for i in $(seq 3); do
     run_server /dev/null
-    run_client $BOTH_METHOD "$OUTPUT_DIR/$i.speed"
+    run_client $CASE.$BOTH_METHOD "$OUTPUT_DIR/$i.speed"
     kill $(jobs -p)
 done
 
 # Collect Compute
 profile_server "$OUTPUT_DIR/profile.sqlite"
 
-run_client $RPC_ONLY_METHOD "$OUTPUT_DIR/com-iter.log"
+run_client $CASE.$RPC_ONLY_METHOD "$OUTPUT_DIR/com-iter.log"
 
 kill $(jobs -p)
