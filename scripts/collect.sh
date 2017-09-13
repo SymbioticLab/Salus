983c397a135b265286edf004f1045384db173a38
Peifeng Yu <peifeng@umich.edu>
2017-09-12 23:16:09 -0400

    Fix collect script

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -5,17 +5,22 @@
 #-------------------------------------------------------
 OUTPUT_DIR=$HOME/vgg16-rpc-gpu
 CASE='test_tf.test_vgg'
-METHOD='TestVgg16.test_rpc_only'
+RPC_ONLY_METHOD='TestVgg16.test_rpc_only'
+BOTH_METHOD='TestVgg16.test_fake_data'
+
+PYTHON=$HOME/.local/venvs/tfbuild/bin/python
 
 #-------------------------------------------------------
 # Implementations
 #-------------------------------------------------------
+set -e
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXEC_DIR=$DIR/..
 
 function run_client() {
     pushd $EXEC_DIR/tests > /dev/null
-    env TF_CPP_MIN_LOG_LEVEL=3 CUDA_VISIBLE_DEVICES=0,1 python -m $CASE $METHOD > "$1"
+    env TF_CPP_MIN_LOG_LEVEL=3 CUDA_VISIBLE_DEVICES=0,1 $PYTHON -m $CASE $1 | tee "$2"
     popd > /dev/null
 }
 
@@ -24,28 +29,28 @@ function run_server() {
 }
 
 function profile_server() {
-    env CUDA_VISIBLE_DEVICES=2,3 nvprof --export-profile "$1" --events active_warps -- $EXEC_DIR/build/src/executor &
+    env TF_CPP_MIN_LOG_LEVEL=4 CUDA_VISIBLE_DEVICES=2,3 nvprof --export-profile "$1" --events active_warps -- $EXEC_DIR/build/src/executor &
 }
 
-mkdir -p "$OUTPUT_DIR"
+rm -rf "$OUTPUT_DIR" && mkdir -p "$OUTPUT_DIR"
 cd $EXEC_DIR
 
 # Collect memory usage
 
 run_server "$OUTPUT_DIR/exec.output"
-run_client "$OUTPUT_DIR/mem-iter.log"
+run_client $RPC_ONLY_METHOD "$OUTPUT_DIR/mem-iter.log"
 kill $(jobs -p)
 
 # Collect JCT
 for i in $(seq 3); do
     run_server /dev/null
-    run_client "$OUTPUT_DIR/$i.speed"
+    run_client $RPC_ONLY_METHOD "$OUTPUT_DIR/$i.speed"
     kill $(jobs -p)
 done
 
 # Collect Compute
 profile_server "$OUTPUT_DIR/profile.sqlite"
 
-run_client "$OUTPUT_DIR/com-iter.log"
+run_client $BOTH_METHOD "$OUTPUT_DIR/com-iter.log"
 
 kill $(jobs -p)
