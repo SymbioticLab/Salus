d441fbd7d1582aa5afc3a16b860204179155e302
Peifeng Yu <peifeng@umich.edu>
2017-10-15 00:30:40 +0000

    Add RelWithDebInfo

--- compile.sh
+++ compile.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 set -e
-BUILD_TYPES=(Debug Release)
+BUILD_TYPES=(Debug Release RelWithDebInfo)
 
 # Configure
 for build_type in ${BUILD_TYPES[@]}; do
+    echo "Configure using build type $build_type"
     binary_tree=build/$build_type
-    [[ -d $binary_tree ]] || cmake -H. -B$binary_tree -DCMAKE_BUILD_TYPE=$biuld_type \
+    [[ -d $binary_tree ]] || cmake -H. -B$binary_tree -DCMAKE_BUILD_TYPE=$build_type \
         -DTENSORFLOW_ROOT=$HOME/buildbed/tensorflow-rpcdev
 done
 
 # Build
 for build_type in ${BUILD_TYPES[@]}; do
+    echo "Building $build_type"
     cmake --build build/$build_type -- -j80
 done
