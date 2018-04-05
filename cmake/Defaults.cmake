b973fa01508e506b3824a0cad2c070f18500de62
Aetf <7437103@gmail.com>
2018-04-04 20:28:22 -0400

    Don't overwrite CMAKE_PREFIX_PATH

--- cmake/Defaults.cmake
+++ cmake/Defaults.cmake
@@ -9,10 +9,8 @@ if(NOT CMAKE_BUILD_TYPE)
 endif()
 
 # Default third party package path
-if(NOT CMAKE_PREFIX_PATH)
-    set(CMAKE_PREFIX_PATH spack-packages)
-endif()
-get_filename_component(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH}" ABSOLUTE BASE_DIR ${PROJECT_SOURCE_DIR})
+list(APPEND CMAKE_PREFIX_PATH spack-packages)
+list(REMOVE_DUPLICATES CMAKE_PREFIX_PATH)
 
 # Use C++14 standard
 set(CMAKE_CXX_STANDARD 17)
