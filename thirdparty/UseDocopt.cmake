9cf226a678a399e9aa3218c43c11f34d9820b6c8
Peifeng Yu <peifeng@umich.edu>
2018-04-08 22:21:44 -0400

    Port thirdparty CMakeLists.txt from split-shared

--- thirdparty/UseDocopt.cmake
+++ thirdparty/UseDocopt.cmake
@@ -6,17 +6,7 @@ set(library_name docopt.cpp)
 
 message("-- ${library_name} version: bundled")
 
-if(NOT EXISTS ${third_party_dir}/${library_name}/CMakeLists.txt)
-    message(STATUS "--   Initialize submodule")
-    execute_process(COMMAND "git" "submodule" "update" "--init" "${third_party_dir}/${library_name}"
-        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-        RESULT_VARIABLE retcode
-    )
-    if(NOT "${retcode}" STREQUAL "0")
-        message(FATAL_ERROR "Failed to checkout ${library_name} as submodule")
-    endif(NOT "${retcode}" STREQUAL "0")
-endif(NOT EXISTS ${third_party_dir}/${library_name}/CMakeLists.txt)
-
 # Set any options
 #set(SPDLOG_BUILD_TESTING OFF CACHE BOOL "Build spdlog tests" FORCE)
-add_subdirectory(${third_party_dir}/${library_name})
+
+add_submodule(${library_name})
--- thirdparty/UseEasyloggingpp.cmake
+++ thirdparty/UseEasyloggingpp.cmake
@@ -1,49 +1,21 @@
-
-# Use CMAKE_CURRENT_LIST_DIR as this file will be included
-set(third_party_dir ${CMAKE_CURRENT_LIST_DIR})
-
 # Library name, also the path
 set(library_name easyloggingpp)
 
 message("-- ${library_name} version: bundled")
 
-if(NOT EXISTS ${third_party_dir}/${library_name}/CMakeLists.txt)
-    message(STATUS "--   Initialize submodule")
-    execute_process(COMMAND "git" "submodule" "update" "--init" "${third_party_dir}/${library_name}"
-        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-        RESULT_VARIABLE retcode
-    )
-    if(NOT "${retcode}" STREQUAL "0")
-        message(FATAL_ERROR "Failed to checkout ${library_name} as submodule")
-    endif(NOT "${retcode}" STREQUAL "0")
-    execute_process(COMMAND "git" "apply" "../vmodule-respect-vlevel-elsewhere.patch"
-        WORKING_DIRECTORY "${third_party_dir}/${library_name}"
-        RESULT_VARIABLE retcode
-    )
-    if(NOT "${retcode}" STREQUAL "0")
-        message(FATAL_ERROR "Failed to patch ${library_name} after submodule initialization")
-    endif(NOT "${retcode}" STREQUAL "0")
-    execute_process(COMMAND "git" "apply" "../more-color-term.patch"
-        WORKING_DIRECTORY "${third_party_dir}/${library_name}"
-        RESULT_VARIABLE retcode
-    )
-    if(NOT "${retcode}" STREQUAL "0")
-        message(FATAL_ERROR "Failed to patch ${library_name} after submodule initialization")
-    endif(NOT "${retcode}" STREQUAL "0")
-    execute_process(COMMAND "git" "apply" "../optimize-vlog-is-on.patch"
-        WORKING_DIRECTORY "${third_party_dir}/${library_name}"
-        RESULT_VARIABLE retcode
-        )
-    if(NOT "${retcode}" STREQUAL "0")
-        message(FATAL_ERROR "Failed to patch ${library_name} after submodule initialization")
-    endif(NOT "${retcode}" STREQUAL "0")
-endif(NOT EXISTS ${third_party_dir}/${library_name}/CMakeLists.txt)
-
 # Set any options
 set(build_static_lib ON CACHE BOOL "Build static lib" FORCE)
 set(lib_utc_datetime ON CACHE BOOL "Use UTC datetime" FORCE)
-add_subdirectory(${third_party_dir}/${library_name})
 
+# Fix policy warnings
+cmake_policy(SET CMP0048 OLD)
+
+add_submodule(${library_name}
+    PATCHES
+    vmodule-respect-vlevel-elsewhere.patch
+    more-color-term.patch
+    disable-warning.patch
+    )
 # Macros to enable extra features in Easylogging++
 set(macros
     ELPP_DEBUG_ASSERT_FAILURE
@@ -55,10 +27,16 @@ set(macros
     ELPP_NO_DEFAULT_LOG_FILE
     ELPP_DISABLE_LOGGING_FLAGS_FROM_ARG
     ELPP_DISABLE_LOG_FILE_FROM_ARG
-)
+    )
+
+# Use CMAKE_CURRENT_LIST_DIR as this file will be included
+set(third_party_dir ${CMAKE_CURRENT_LIST_DIR})
 
 # Make the target suitable for directly use
 set_property(TARGET easyloggingpp APPEND PROPERTY INCLUDE_DIRECTORIES "${third_party_dir}/${library_name}/src")
 set_property(TARGET easyloggingpp APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${third_party_dir}/${library_name}/src")
 set_property(TARGET easyloggingpp APPEND PROPERTY COMPILE_DEFINITIONS ${macros})
 set_property(TARGET easyloggingpp APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS ${macros})
+# Since the target is defined inside a nested project() call, global CMAKE_POSITION_INDENPENDENT_CODE has no effect on it.
+# re-enable it here.
+set_property(TARGET easyloggingpp PROPERTY POSITION_INDEPENDENT_CODE ON)
