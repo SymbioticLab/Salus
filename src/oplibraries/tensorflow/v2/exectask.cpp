098d7297ddcc37494f05fc43420e37775f79bbe1
Peifeng Yu <peifeng@umich.edu>
2018-07-13 22:44:11 -0400

    Cleanup ExecTask resource usage estimation code

--- src/oplibraries/tensorflow/v2/exectask.cpp
+++ src/oplibraries/tensorflow/v2/exectask.cpp
@@ -527,7 +527,7 @@ void ExecTask::afterRun(const tf::Status &s, const Callbacks &cbs)
 {
     DCHECK(ditem.device);
     if (s.ok()) {
-        // save succeed estimation
+        // save succeed peak resource usage as an estimation
         m_state->impl_->saveSucceedUsageForNode(item, resourceContext().spec().type,
                                                 ditem.device->peakResourceUsage());
     }
@@ -558,10 +558,12 @@ bool ExecTask::maybeMemoryFailure(const tf::Status &s, const MemFailCallback &me
 
         DCHECK(ditem.device);
         resources::merge(failedAlloc, ditem.device->failedResourceRequest());
+        resources::removeInvalid(failedAlloc);
 
-        if (failureTimes > 10) {
-            LOG(WARNING) << "Failed more than 10 times: " << failureTimes <<
-                         " current estimation:" << estimatedUsage(devices::GPU0)
+        if (failureTimes == 11) {
+            auto eu = estimatedUsage(devices::GPU0);
+            LOG(WARNING) << "Failed more than 10 times: " << failureTimes
+                         << " current estimation:" << eu
                          << " total failed request: " << failedAlloc << " " << DebugString();
         }
 
--- src/oplibraries/tensorflow/v2/tfallocator.cpp
+++ src/oplibraries/tensorflow/v2/tfallocator.cpp
@@ -219,10 +219,15 @@ bool PerOpAllocator::ShouldAllocateEmptyTensors()
 void PerOpAllocator::recordSize(void *ptr, size_t size, bool didRollback)
 {
     auto g = sstl::with_guard(m_mu);
-    if (!ptr && !didRollback) {
-        // Only remember failed alloc size if we didn't rollback, which
-        // means our estimation was enough, but the underlaying allocator failed
-        // due to some other reason.
+    if (!ptr) {
+        if (didRollback) {
+            // we did a wrong resource estimation and the actual allocator didn't allocate enough memory for this request
+            // probably due to mem fragmentation
+            ++m_mismatchedResRequest;
+            return;
+        }
+
+        // otherwise remember the failed alloc size so it can be picked by by ExecTask to refine the estimation
         m_lastFailedAllocSize = size;
     }
     if (!ptr) {
