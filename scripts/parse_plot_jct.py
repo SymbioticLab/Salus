fa10193ce6a9704d5cb92bf35b2d3c7acc151542
Peifeng Yu <peifeng@umich.edu>
2018-09-03 03:41:57 -0400

    Use TFDist as baseline

--- scripts/parse_plot_jct.py
+++ scripts/parse_plot_jct.py
@@ -34,8 +34,9 @@ tf, salus, tfdist = load_exp17(path)
 
 column = '20iter-avg'
 pits = pd.DataFrame({
-    'Salus': salus[column] / tf[column],
-    # 'TFDist': tfdist[column] / tf[column]
+    'Salus': salus[column] / tfdist[column],
+    # 'TFDist': tfdist[column] / tf[column],
+    # 'Salus/TFDist': salus[column] / tfdist[column],
 })
 
 pits = pits[~pits.index.str.startswith('mnist')]
@@ -43,14 +44,14 @@ pits = pits[~pits.index.str.startswith('mnist')]
 with plt.style.context(['seaborn-paper', 'mypaper']):
     ax = pits.plot.bar(legend=None)
     pu.axhlines(1.0, ax=ax, color='r', linestyle='--', linewidth=.5)
-    
-    ax.set_ylim(0.9, 1.25)
+
+    ax.set_ylim(0.9, 2)
     ax.set_xlabel('Workloads')
     ax.set_ylabel('Normalized Per Iteration\nTraining Time')
     # ax.legend()
-    
+
     ax.tick_params(axis='x', labelsize=7)
-    
+
     ax.figure.set_size_inches(4.7, 2.35, forward=True)
     ax.figure.tight_layout()
     ax.figure.savefig('/tmp/workspace/exp17.pdf', dpi=300)
