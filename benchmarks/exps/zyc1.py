751935c1d6ae418563a7dd4e3c2d6f164443d4f7
zhongyuchen <izhongyuchen@gmail.com>
2019-08-22 11:14:22 +0800

    Remove unused code

--- benchmarks/exps/zyc1.py
+++ benchmarks/exps/zyc1.py
@@ -57,78 +57,8 @@ def case(policy):
             *workload_list
             )
 
-def case1():
-    scfg = maybe_forced_preset(presets.MostEfficient)
-    scfg.scheduler = 'fair'
-
-    run_seq(scfg.copy(output_dir=FLAGS.save_dir/'case1'),
-            WTL.create("inception3eval", 50, 250),
-            Pause(5),
-            WTL.create("inception3eval", 50, 250),
-            Pause(5),
-            WTL.create("inception3eval", 50, 250),
-            Pause(5),
-            WTL.create("inception3eval", 50, 250),
-            Pause(5),
-            WTL.create("inception3eval", 50, 250),
-            )
-
-
-def case2():
-    scfg = maybe_forced_preset(presets.MostEfficient)
-    scfg.scheduler = 'pack'
-    scfg.logconf = 'log'
-    scfg.env['SALUS_DISABLE_SHARED_LANE'] = '1'
-
-    run_seq(scfg.copy(output_dir=FLAGS.save_dir/'case2'),
-            WTL.create("inception3eval", 50, 250),
-            Pause(10),
-            WTL.create("inception3eval", 50, 250),
-            Pause(10),
-            WTL.create("inception3eval", 50, 250),
-            )
-
-
-def case3():
-    scfg = maybe_forced_preset(presets.MostEfficient)
-    scfg.scheduler = 'fair'
-    scfg.env['SALUS_DISABLE_LANEMGR'] = '1'
-
-    run_seq(scfg.copy(output_dir=FLAGS.save_dir/'case3'),
-            WTL.create("inception3eval", 50, 250),
-            Pause(10),
-            WTL.create("inception3eval", 50, 250),
-            Pause(10),
-            WTL.create("inception3eval", 50, 250),
-            )
-
-
-def test():
-    """Measure TF inference memory usage"""
-    wl = WTL.create("inception3eval", 50, 50, executor=Executor.TF)
-    wl.env['TF_CPP_MIN_VLOG_LEVEL'] = '1'
-    wl.env['TF_CPP_MIN_LOG_LEVEL'] = ''
-    outdir = FLAGS.save_dir/'test'/wl.canonical_name
-    run_tf(outdir, wl)
-
-    # filter and move file to a more convinent name
-    for f in outdir.iterdir():
-        with f.with_name('alloc.output').open('w') as file:
-            grep = execute(['egrep', r"] (\+|-)", f.name], stdout=file, cwd=str(f.parent))
-            grep.wait()
-        f.unlink()
-        break
 
 
 def main(argv):
-    '''
-    command = argv[0] if argv else "case1"
-
-    {
-        "case1": case1,
-        "case2": case2,
-        "case3": case3,
-        "test": test,
-    }[command]()'''
     policy = argv[0]
     case(policy)
