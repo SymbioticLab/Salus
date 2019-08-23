991d2cdd3e4abf1c621758bda0310812ee825632
zhongyuchen <izhongyuchen@gmail.com>
2019-08-23 21:44:58 +0800

    add exp4

---
+++ benchmarks/exps/zyc4.py
@@ -0,0 +1,48 @@
+'''
+Created Date: Friday, August 23rd 2019, 9:43:52 pm
+Author: Yuchen Zhong
+Email: yczhong@hku.hk
+'''
+
+from __future__ import absolute_import, print_function, division, unicode_literals
+
+import os
+from absl import flags
+
+from benchmarks.driver.runner import Executor
+from benchmarks.driver.server.config import presets
+from benchmarks.driver.utils import execute
+from benchmarks.driver.workload import WTL
+from benchmarks.exps import run_seq, maybe_forced_preset, Pause, run_tf, run_tfdist
+
+
+FLAGS = flags.FLAGS
+
+def case(policy):
+    scfg = maybe_forced_preset(presets.MostEfficient)
+    scfg.scheduler = policy
+    if policy == "mix":
+        os.environ["SALUS_TIMEOUT"] = "666666"
+    else:
+        os.environ["SALUS_TIMEOUT"] = "0"
+
+    folder_name = policy
+    workload_list = [
+        WTL.create("inception3", 25, 250), # training
+        WTL.create("inception3eval", 1, 2000)
+    ]
+    run_seq(scfg.copy(output_dir=FLAGS.save_dir/folder_name),
+            *workload_list
+            )
+
+
+
+def main(argv):
+    policy_list = [
+        "pack",
+        "fair",
+        "preempt",
+        "mix"
+    ]
+    for policy in policy_list:
+        case(policy)
---
+++ scripts/parse_zyc3.py
@@ -0,0 +1,74 @@
+'''
+Created Date: Friday, August 23rd 2019, 9:25:48 pm
+Author: Yuchen Zhong
+Email: yczhong@hku.hk
+'''
+import re
+import os 
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+training_job_name = "alexnet_25.salus.250iter.0.output"
+inference_job_names = ["resnet50eval_1.salus.500iter.1.output",
+                       "resnet50eval_1.salus.500iter.2.output" 
+                       "resnet50eval_1.salus.500iter.3.output" 
+                       ]
+log_path = "scripts/templogs/zyc3"
+policy_list = [
+    "pack", "fair", "preempt", "mix"
+]
+
+font = {'family' : 'normal',
+        'weight' : 'bold',
+        'size'   : 22}
+
+plt.rc('font', **font)
+
+if __name__=="__main__":   
+    ## training  
+    lines = []
+    fig = plt.figure(figsize=(15,15))
+    for policy in policy_list:
+        path = os.path.join(log_path, policy)
+        with open(os.path.join(path, training_job_name), "r") as f:
+            content = f.read()
+        pattern = re.compile(r"images/sec: \d+.\d ")
+        items = pattern.findall(content)
+        # print(len(results))
+        speeds = list(map(lambda str: float(str.split()[1].strip()), items))
+
+        min_v, max_v = int(min(speeds)), int(max(speeds))
+        plt.yticks(np.arange(min_v, max_v, 20))
+        line, = plt.plot(speeds, linewidth=5.0)
+        lines.append(line)
+    plt.legend(lines, policy_list, prop={'size':30})
+    plt.title("exp3 - " + training_job_name)
+    plt.xlabel("iter")
+    plt.ylabel("images/sec")
+    # plt.show()
+    os.system("mkdir -p scripts/imgs/exp3")
+    plt.savefig('scripts/imgs/exp3/' + training_job_name + ".png", dpi=fig.dpi)
+
+    ## inference 
+    for policy in policy_list:
+        path = os.path.join(log_path, policy)
+        items = []
+        for name in inference_job_names:
+            with open(os.path.join(path, inference_job_names[0]), "r") as f:
+                content = f.read()
+            pattern = re.compile(r"\d+\.\d ")
+            items.extend(pattern.findall(content)[100:])
+        # print(len(results))
+        speeds = list(map(lambda str: float(str.strip()), items))
+        latency = list(map(lambda x: 1000/x, speeds))
+        
+        min_v = np.min(latency)
+        max_v = np.max(latency)
+        avg_v = np.mean(latency)
+        sorted_list = sorted(latency)
+        tail_v = sorted_list[int(len(sorted_list)*0.99)]
+
+        print("%s %.2f %.2f %.2f %.2f" % (
+            policy, min_v, max_v, avg_v, tail_v
+        ))
